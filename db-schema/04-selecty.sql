7b2032f6030eb6241aa0c5c87f76acb3c783216b
Richard Hulek <hulek@iba.muni.cz>
2014-05-09 09:46:04 +0200

    Select pro obrazovku Program

--- db-schema/04-selecty.sql
+++ db-schema/04-selecty.sql
@@ -1,44 +1,50 @@
 ﻿SELECT
-  projection.projection_id,
-  movie.title,
-  movie.descritption,
-  movie.running_time,
-  date_part('year', movie.release_date),
-  genre.genre_type,
-  projection.start,
-  projection.price,
-  projection.projection_id,
-  hall.hall_id
+  projection.projection_id as "projection_id",
+  movie.title AS "title",
+  movie.descritption AS "descritiption",
+  movie.running_time AS "running_time",
+  date_part('year', movie.release_date) AS "release_year",
+  projection.start AS "projection_start_time",
+  projection.price AS "projection_price",
+  hall.hall_id AS "hall_id",
+  (SELECT
+    array(
+      SELECT
+        genre.genre_type
+      FROM
+        movie_genre
+        LEFT JOIN genre
+          ON genre.genre_id = movie_genre.genre_id
+      WHERE
+        movie.movie_id = movie_genre.movie_id
+    )
+  )AS "genre_types",
+  (
+  SELECT
+    COUNT(*)
+  FROM
+    hall_seat
+    LEFT JOIN projection pr
+      ON pr.hall_id = hall_seat.hall_id
+  WHERE
+    pr.projection_id = projection.projection_id
+  ) AS "seats_total",
+  (
+  SELECT
+    COUNT(*)
+  FROM
+    booking
+    LEFT JOIN projection pr
+      ON pr.projection_id = booking.projection_id
+  WHERE
+    pr.projection_id = projection.projection_id
+  ) AS "seats_booked"
+  
 FROM 
   projection
   LEFT JOIN movie
     ON movie.movie_id = projection.movie_id
-  LEFT JOIN movie_genre
-    ON movie_genre.movie_id = movie.movie_id
-  LEFT JOIN genre
-    ON genre.genre_id = movie_genre.genre_id
   LEFT JOIN hall
     ON hall.hall_id = projection.hall_id
   LEFT JOIN branch_office
     ON branch_office.branch_office_id = hall.branch_office_id
-
-
--- pocet mist k sezeni
-SELECT
-  COUNT(*)
-FROM
-  hall_seat
-  LEFT JOIN projection
-    ON projection.hall_id = hall_seat.hall_id
-WHERE
-  projection.projection_id = 1
-
--- pocet rezervaci
-SELECT
-  COUNT(*)
-FROM
-  booking
-  LEFT JOIN projection
-    ON projection.projection_id = booking.projection_id
-WHERE
-  projection.projection_id = 1
