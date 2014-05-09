e5c7ab5ee3288670360b9a7d44e584154b543531
edee111 <324681@mail.muni.cz>
2014-05-09 14:01:02 +0200

    structure.sql - primarni klice

--- db-schema/structure.sql
+++ db-schema/structure.sql
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Navicat PGSQL Data Transfer
 
 Source Server         : localhost
@@ -14,178 +14,12 @@ File Encoding         : 65001
 Date: 2014-05-07 11:45:50
 */
 
-
--- ----------------------------
--- Sequence structure for booking_booking_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."booking_booking_id_seq";
-CREATE SEQUENCE "public"."booking_booking_id_seq"
- INCREMENT 1
- MINVALUE 1
- MAXVALUE 9223372036854775807
- START 2
- CACHE 1;
-SELECT setval('"public"."booking_booking_id_seq"', 2, true);
-
--- ----------------------------
--- Sequence structure for booking_status_booking_status_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."booking_status_booking_status_id_seq";
-CREATE SEQUENCE "public"."booking_status_booking_status_id_seq"
- INCREMENT 1
- MINVALUE 1
- MAXVALUE 9223372036854775807
- START 3
- CACHE 1;
-SELECT setval('"public"."booking_status_booking_status_id_seq"', 3, true);
-
--- ----------------------------
--- Sequence structure for branch_office_branch_office_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."branch_office_branch_office_id_seq";
-CREATE SEQUENCE "public"."branch_office_branch_office_id_seq"
- INCREMENT 1
- MINVALUE 1
- MAXVALUE 9223372036854775807
- START 1
- CACHE 1;
-SELECT setval('"public"."branch_office_branch_office_id_seq"', 1, true);
-
--- ----------------------------
--- Sequence structure for club_club_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."club_club_id_seq";
-CREATE SEQUENCE "public"."club_club_id_seq"
- INCREMENT 1
- MINVALUE 1
- MAXVALUE 9223372036854775807
- START 1
- CACHE 1;
-
--- ----------------------------
--- Sequence structure for country_country_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."country_country_id_seq";
-CREATE SEQUENCE "public"."country_country_id_seq"
- INCREMENT 1
- MINVALUE 1
- MAXVALUE 9223372036854775807
- START 1
- CACHE 1;
-SELECT setval('"public"."country_country_id_seq"', 1, true);
-
--- ----------------------------
--- Sequence structure for customer_customer_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."customer_customer_id_seq";
-CREATE SEQUENCE "public"."customer_customer_id_seq"
- INCREMENT 1
- MINVALUE 1
- MAXVALUE 9223372036854775807
- START 1
- CACHE 1;
-SELECT setval('"public"."customer_customer_id_seq"', 1, true);
-
--- ----------------------------
--- Sequence structure for employee_eymploee_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."employee_eymploee_id_seq";
-CREATE SEQUENCE "public"."employee_eymploee_id_seq"
- INCREMENT 1
- MINVALUE 1
- MAXVALUE 9223372036854775807
- START 1
- CACHE 1;
-
--- ----------------------------
--- Sequence structure for genre_genre_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."genre_genre_id_seq";
-CREATE SEQUENCE "public"."genre_genre_id_seq"
- INCREMENT 1
- MINVALUE 1
- MAXVALUE 9223372036854775807
- START 1
- CACHE 1;
-SELECT setval('"public"."genre_genre_id_seq"', 1, true);
-
--- ----------------------------
--- Sequence structure for hall_hall_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."hall_hall_id_seq";
-CREATE SEQUENCE "public"."hall_hall_id_seq"
- INCREMENT 1
- MINVALUE 1
- MAXVALUE 9223372036854775807
- START 4
- CACHE 1;
-SELECT setval('"public"."hall_hall_id_seq"', 4, true);
-
--- ----------------------------
--- Sequence structure for hall_property_hall_property_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."hall_property_hall_property_id_seq";
-CREATE SEQUENCE "public"."hall_property_hall_property_id_seq"
- INCREMENT 1
- MINVALUE 1
- MAXVALUE 9223372036854775807
- START 1
- CACHE 1;
-
--- ----------------------------
--- Sequence structure for hall_seat_seat_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."hall_seat_seat_id_seq";
-CREATE SEQUENCE "public"."hall_seat_seat_id_seq"
- INCREMENT 1
- MINVALUE 1
- MAXVALUE 9223372036854775807
- START 20
- CACHE 1;
-SELECT setval('"public"."hall_seat_seat_id_seq"', 20, true);
-
--- ----------------------------
--- Sequence structure for movie_movie_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."movie_movie_id_seq";
-CREATE SEQUENCE "public"."movie_movie_id_seq"
- INCREMENT 1
- MINVALUE 1
- MAXVALUE 9223372036854775807
- START 1
- CACHE 1;
-SELECT setval('"public"."movie_movie_id_seq"', 1, true);
-
--- ----------------------------
--- Sequence structure for projection_projection_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."projection_projection_id_seq";
-CREATE SEQUENCE "public"."projection_projection_id_seq"
- INCREMENT 1
- MINVALUE 1
- MAXVALUE 9223372036854775807
- START 1
- CACHE 1;
-SELECT setval('"public"."projection_projection_id_seq"', 1, true);
-
--- ----------------------------
--- Sequence structure for seat_type_seat_type_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."seat_type_seat_type_id_seq";
-CREATE SEQUENCE "public"."seat_type_seat_type_id_seq"
- INCREMENT 1
- MINVALUE 1
- MAXVALUE 9223372036854775807
- START 2
- CACHE 1;
-SELECT setval('"public"."seat_type_seat_type_id_seq"', 2, true);
-
 -- ----------------------------
 -- Table structure for booking
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."booking";
 CREATE TABLE "public"."booking" (
-"booking_id" integer DEFAULT nextval('booking_booking_id_seq'::regclass) NOT NULL,
+"booking_id" serial primary key,
 "projection_id" integer,
 "customer_id" integer,
 "booking_status_id" integer,
@@ -202,8 +36,8 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."booking_hall_seat";
 CREATE TABLE "public"."booking_hall_seat" (
-"booking_id" integer,
-"seat_id" integer
+"booking_id" integer NOT NULL,
+"seat_id" integer NOT NULL
 )
 WITH (OIDS=FALSE)
 
@@ -214,7 +48,7 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."booking_status";
 CREATE TABLE "public"."booking_status" (
-"booking_status_id" integer DEFAULT nextval('booking_status_booking_status_id_seq'::regclass) NOT NULL,
+"booking_status_id" serial primary key,
 "status" varchar(255) COLLATE "default"
 )
 WITH (OIDS=FALSE)
@@ -226,7 +60,7 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."branch_office";
 CREATE TABLE "public"."branch_office" (
-"branch_office_id" integer DEFAULT nextval('branch_office_branch_office_id_seq'::regclass) NOT NULL,
+"branch_office_id" serial primary key,
 "name" character varying(255) COLLATE "default",
 "description" text COLLATE "default",
 "address_line_1" character varying(255) COLLATE "default",
@@ -244,7 +78,7 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."club";
 CREATE TABLE "public"."club" (
-"club_id" integer DEFAULT nextval('club_club_id_seq'::regclass) NOT NULL,
+"club_id" serial primary key,
 "name" character varying(255) COLLATE "default",
 "sale_amount" integer,
 "sale_percentage" numeric(3,2)
@@ -258,7 +92,7 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."country";
 CREATE TABLE "public"."country" (
-"country_id" integer DEFAULT nextval('country_country_id_seq'::regclass) NOT NULL,
+"country_id" serial primary key,
 "name" character varying(255) COLLATE "default" NOT NULL
 )
 WITH (OIDS=FALSE)
@@ -270,7 +104,7 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."customer";
 CREATE TABLE "public"."customer" (
-"customer_id" integer DEFAULT nextval('customer_customer_id_seq'::regclass) NOT NULL,
+"customer_id" serial primary key,
 "first_name" character varying(128) COLLATE "default",
 "last_name" character varying(128) COLLATE "default",
 "phone_number" character varying(15) COLLATE "default",
@@ -287,7 +121,7 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."employee";
 CREATE TABLE "public"."employee" (
-"employee_id" integer DEFAULT nextval('employee_eymploee_id_seq'::regclass) NOT NULL,
+"employee_id" serial primary key,
 "username" character varying(255) COLLATE "default" NOT NULL,
 "password" character varying(255) COLLATE "default" NOT NULL
 )
@@ -300,7 +134,7 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."genre";
 CREATE TABLE "public"."genre" (
-"genre_id" integer DEFAULT nextval('genre_genre_id_seq'::regclass) NOT NULL,
+"genre_id" serial primary key,
 "genre_type" character varying(255) COLLATE "default"
 )
 WITH (OIDS=FALSE)
@@ -312,7 +146,7 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."hall";
 CREATE TABLE "public"."hall" (
-"hall_id" integer DEFAULT nextval('hall_hall_id_seq'::regclass) NOT NULL,
+"hall_id" serial primary key,
 "branch_office_id" integer NOT NULL,
 "label" character varying(255) COLLATE "default" NOT NULL
 )
@@ -325,8 +159,8 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."hall_hall_property";
 CREATE TABLE "public"."hall_hall_property" (
-"hall_property_id" integer,
-"hall_id" integer
+"hall_property_id" integer NOT NULL,
+"hall_id" integer NOT NULL
 )
 WITH (OIDS=FALSE)
 
@@ -337,7 +171,7 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."hall_property";
 CREATE TABLE "public"."hall_property" (
-"hall_property_id" integer DEFAULT nextval('hall_property_hall_property_id_seq'::regclass) NOT NULL,
+"hall_property_id" serial primary key,
 "key" character varying(255) COLLATE "default" NOT NULL,
 "value" character varying(255) COLLATE "default" NOT NULL
 )
@@ -350,7 +184,7 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."hall_seat";
 CREATE TABLE "public"."hall_seat" (
-"seat_id" integer DEFAULT nextval('hall_seat_seat_id_seq'::regclass) NOT NULL,
+"seat_id"  serial primary key,
 "hall_id" integer,
 "seat_type_id" integer,
 "seat_row" integer,
@@ -365,7 +199,7 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."movie";
 CREATE TABLE "public"."movie" (
-"movie_id" integer DEFAULT nextval('movie_movie_id_seq'::regclass) NOT NULL,
+"movie_id" serial primary key,
 "title" character varying(255) COLLATE "default",
 "descritption" text COLLATE "default",
 "running_time" integer,
@@ -392,7 +226,7 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."projection";
 CREATE TABLE "public"."projection" (
-"projection_id" integer DEFAULT nextval('projection_projection_id_seq'::regclass) NOT NULL,
+"projection_id" serial primary key,
 "movie_id" integer,
 "hall_id" integer,
 "start" timestamp with time zone,
@@ -408,7 +242,7 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."seat_type";
 CREATE TABLE "public"."seat_type" (
-"seat_type_id" integer DEFAULT nextval('seat_type_seat_type_id_seq'::regclass) NOT NULL,
+"seat_type_id" serial primary key,
 "seat_type" character varying(255) COLLATE "default" NOT NULL,
 "seat_type_sale_amount" integer,
 "seat_type_sale_percantage" numeric(3,2)
@@ -424,34 +258,11 @@ CREATE OR REPLACE VIEW "public"."projection_seats" AS
 SELECT hs.seat_id, hs.seat_type_id, hs.seat_row, hs.seat_number, CASE WHEN (b.booking_id IS NULL) THEN 'free'::text ELSE 'booked'::text END AS state FROM ((hall_seat hs LEFT JOIN projection p USING (hall_id)) LEFT JOIN (SELECT bhs.booking_id, bhs.seat_id FROM (booking_hall_seat bhs JOIN booking ON (((booking.booking_id = bhs.booking_id) AND (booking.booking_status_id <> 1))))) b USING (seat_id)) ORDER BY hs.seat_row, hs.seat_number;
 
 -- ----------------------------
--- Alter Sequences Owned By 
--- ----------------------------
-ALTER SEQUENCE "public"."booking_booking_id_seq" OWNED BY "booking"."booking_id";
-ALTER SEQUENCE "public"."booking_status_booking_status_id_seq" OWNED BY "booking_status"."booking_status_id";
-ALTER SEQUENCE "public"."branch_office_branch_office_id_seq" OWNED BY "branch_office"."branch_office_id";
-ALTER SEQUENCE "public"."club_club_id_seq" OWNED BY "club"."club_id";
-ALTER SEQUENCE "public"."country_country_id_seq" OWNED BY "country"."country_id";
-ALTER SEQUENCE "public"."customer_customer_id_seq" OWNED BY "customer"."customer_id";
-ALTER SEQUENCE "public"."employee_eymploee_id_seq" OWNED BY "employee"."employee_id";
-ALTER SEQUENCE "public"."genre_genre_id_seq" OWNED BY "genre"."genre_id";
-ALTER SEQUENCE "public"."hall_hall_id_seq" OWNED BY "hall"."hall_id";
-ALTER SEQUENCE "public"."hall_property_hall_property_id_seq" OWNED BY "hall_property"."hall_property_id";
-ALTER SEQUENCE "public"."hall_seat_seat_id_seq" OWNED BY "hall_seat"."seat_id";
-ALTER SEQUENCE "public"."movie_movie_id_seq" OWNED BY "movie"."movie_id";
-ALTER SEQUENCE "public"."projection_projection_id_seq" OWNED BY "projection"."projection_id";
-ALTER SEQUENCE "public"."seat_type_seat_type_id_seq" OWNED BY "seat_type"."seat_type_id";
-
--- ----------------------------
 -- Indexes structure for table booking
 -- ----------------------------
 CREATE INDEX "booking_booking_status_id_idx" ON "public"."booking" USING btree (booking_status_id);
 CREATE INDEX "booking_customer_id_idx" ON "public"."booking" USING btree (customer_id);
 CREATE INDEX "booking_projection_id_idx" ON "public"."booking" USING btree (projection_id);
-
--- ----------------------------
--- Primary Key structure for table booking
--- ----------------------------
-ALTER TABLE "public"."booking" ADD PRIMARY KEY ("booking_id");
 
 -- ----------------------------
 -- Indexes structure for table booking_hall_seat
@@ -465,39 +276,9 @@ CREATE INDEX "booking_hall_seat_seat_id_idx" ON "public"."booking_hall_seat" USI
 ALTER TABLE "public"."booking_status" ADD UNIQUE ("status");
 
 -- ----------------------------
--- Primary Key structure for table booking_status
--- ----------------------------
-ALTER TABLE "public"."booking_status" ADD PRIMARY KEY ("booking_status_id");
-
--- ----------------------------
--- Primary Key structure for table branch_office
--- ----------------------------
-ALTER TABLE "public"."branch_office" ADD PRIMARY KEY ("branch_office_id");
-
--- ----------------------------
--- Primary Key structure for table club
--- ----------------------------
-ALTER TABLE "public"."club" ADD PRIMARY KEY ("club_id");
-
--- ----------------------------
--- Primary Key structure for table country
--- ----------------------------
-ALTER TABLE "public"."country" ADD PRIMARY KEY ("country_id");
-
--- ----------------------------
 -- Indexes structure for table customer
 -- ----------------------------
 CREATE INDEX "customer_club_id_idx" ON "public"."customer" USING btree (club_id);
-
--- ----------------------------
--- Primary Key structure for table customer
--- ----------------------------
-ALTER TABLE "public"."customer" ADD PRIMARY KEY ("customer_id");
-
--- ----------------------------
--- Primary Key structure for table employee
--- ----------------------------
-ALTER TABLE "public"."employee" ADD PRIMARY KEY ("employee_id");
 
 -- ----------------------------
 -- Uniques structure for table genre
@@ -505,19 +286,9 @@ ALTER TABLE "public"."employee" ADD PRIMARY KEY ("employee_id");
 ALTER TABLE "public"."genre" ADD UNIQUE ("genre_type");
 
 -- ----------------------------
--- Primary Key structure for table genre
--- ----------------------------
-ALTER TABLE "public"."genre" ADD PRIMARY KEY ("genre_id");
-
--- ----------------------------
 -- Indexes structure for table hall
 -- ----------------------------
 CREATE INDEX "hall_branch_office_id_idx" ON "public"."hall" USING btree (branch_office_id);
-
--- ----------------------------
--- Primary Key structure for table hall
--- ----------------------------
-ALTER TABLE "public"."hall" ADD PRIMARY KEY ("hall_id");
 
 -- ----------------------------
 -- Indexes structure for table hall_hall_property
@@ -531,46 +302,16 @@ CREATE INDEX "hall_hall_property_hall_property_id_idx" ON "public"."hall_hall_pr
 ALTER TABLE "public"."hall_property" ADD UNIQUE ("key");
 
 -- ----------------------------
--- Primary Key structure for table hall_property
--- ----------------------------
-ALTER TABLE "public"."hall_property" ADD PRIMARY KEY ("hall_property_id");
-
--- ----------------------------
 -- Indexes structure for table hall_seat
 -- ----------------------------
 CREATE INDEX "hall_seat_hall_id_idx" ON "public"."hall_seat" USING btree (hall_id);
 CREATE INDEX "hall_seat_seat_type_id_idx" ON "public"."hall_seat" USING btree (seat_type_id);
 
 -- ----------------------------
--- Primary Key structure for table hall_seat
--- ----------------------------
-ALTER TABLE "public"."hall_seat" ADD PRIMARY KEY ("seat_id");
-
--- ----------------------------
--- Primary Key structure for table movie
--- ----------------------------
-ALTER TABLE "public"."movie" ADD PRIMARY KEY ("movie_id");
-
--- ----------------------------
--- Primary Key structure for table movie_genre
--- ----------------------------
-ALTER TABLE "public"."movie_genre" ADD PRIMARY KEY ("movie_id", "genre_id");
-
--- ----------------------------
 -- Indexes structure for table projection
 -- ----------------------------
 CREATE INDEX "projection_hall_id_idx" ON "public"."projection" USING btree (hall_id);
 CREATE INDEX "projection_movie_id_idx" ON "public"."projection" USING btree (movie_id);
-
--- ----------------------------
--- Primary Key structure for table projection
--- ----------------------------
-ALTER TABLE "public"."projection" ADD PRIMARY KEY ("projection_id");
-
--- ----------------------------
--- Primary Key structure for table seat_type
--- ----------------------------
-ALTER TABLE "public"."seat_type" ADD PRIMARY KEY ("seat_type_id");
 
 -- ----------------------------
 -- Foreign Key structure for table "public"."booking"
