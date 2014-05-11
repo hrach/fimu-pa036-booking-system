e90aadd8f1f99c7be3e6536dd56388db479411bf
hrach <hrach.cz@gmail.com>
2014-05-11 22:01:30 +0200

    typos

--- app-dev-root/src/PA032/Bundle/MultikinoBundle/VO/ProgramRecordVO.php
+++ app-dev-root/src/PA032/Bundle/MultikinoBundle/VO/ProgramRecordVO.php
@@ -4,78 +4,44 @@ namespace PA032\Bundle\MultikinoBundle\VO;
 
 class ProgramRecordVO
 {
-	/**
-	 * 
-	 * @var int
-	 */
+	/** @var int */
 	public $projectionId;
-	
-	/**
-	 * 
-	 * @var string
-	 */
+
+	/** @var string */
 	public $title;
-	
-	/**
-	 * 
-	 * @var string
-	 */
+
+	/** @var string */
 	public $descritiption;
-	
-	/**
-	 * 
-	 * @var int
-	 */
+
+	/** @var int */
 	public $runningTime;
-	
-	/**
-	 * 
-	 * @var int
-	 */
+
+	/** @var int */
 	public $releaseYear;
-	
-	/**
-	 * 
-	 * @var \DateTime
-	 */
+
+	/** @var \DateTime */
 	public $projectionStartTime;
-	
-	/**
-	 * 
-	 * @var real
-	 */
+
+	/** @var double */
 	public $projectionPrice;
-	
-	/**
-	 * 
-	 * @var int
-	 */
+
+	/** @var int */
 	public $hallId;
-	
-	/**
-	 * 
-	 * @var string[]
-	 */
+
+	/** @var string[] */
 	public $genreTypes;
-	
-	/**
-	 * 
-	 * @var int
-	 */
+
+	/** @var int */
 	public $seatsTotal;
-	
-	/**
-	 * 
-	 * @var int
-	 */
+
+	/** @var int */
 	public $seatsBooked;
-	
+
 	/**
 	 * Pocet minut od pulnoci
 	 * @var int
 	 */
 	public $minuteOffsetFromMidnight;
-	
+
 }
 
-?>
