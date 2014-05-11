1603f5ed60a223460d6f8d8f507eab30b5265e31
hrach <hrach.cz@gmail.com>
2014-05-11 22:20:36 +0200

    Projection schedule refactored into view

--- app-dev-root/src/PA032/Bundle/MultikinoBundle/DAO/ProjectionDAO.php
+++ app-dev-root/src/PA032/Bundle/MultikinoBundle/DAO/ProjectionDAO.php
@@ -16,43 +16,19 @@ class ProjectionDAO extends DAOBaseImpl
 	{
 		$records = array();
 
-		try
-		{
-			$stmt = $this->db->prepare('
-					SELECT DISTINCT
-					  hall.branch_office_id as "branch_office_id",
-					  date_part(\'year\', projection.start) AS "projection_date_year",
-					  date_part(\'month\', projection.start) AS "projection_date_month",
-					  date_part(\'day\', projection.start) AS "projection_date_day"
-					FROM
-					  projection
-					  LEFT JOIN movie
-					    ON movie.movie_id = projection.movie_id
-					  LEFT JOIN hall
-					    ON hall.hall_id = projection.hall_id
-					  LEFT JOIN branch_office
-					    ON branch_office.branch_office_id = hall.branch_office_id
-					WHERE
-					  branch_office.branch_office_id = :branch_office_id
-					ORDER BY
-					  projection_date_year,
-					  projection_date_month,
-					  projection_date_day
-					');
-			$stmt->bindParam(':branch_office_id', $branchOfficeId);
-			$stmt->execute();
+		$stmt = $this->db->prepare('
+			SELECT *
+			FROM projection_schedule
+			WHERE
+				branch_office_id = :branch_office_id
+		');
+		$stmt->bindParam(':branch_office_id', $branchOfficeId);
+		$stmt->execute();
 
-			while ($row = $stmt->fetch(\PDO::FETCH_ASSOC, \PDO::FETCH_ORI_NEXT))
-			{
-				$date = new DateTime();
-				$date->setDate($row["projection_date_year"], $row["projection_date_month"], $row["projection_date_day"]);
-
-				array_push($records, $date);
-			}
-		}
-		catch (\Exception $e)
-		{
-			throw new \Exception($e);
+		while ($row = $stmt->fetch(\PDO::FETCH_ASSOC, \PDO::FETCH_ORI_NEXT)) {
+			$date = new DateTime();
+			$date->setDate($row["projection_date_year"], $row["projection_date_month"], $row["projection_date_day"]);
+			$records[] = $date;
 		}
 
 		return $records;
