0ae3bc6073826fdd653e771272a525ed78d512d9
hrach <hrach.cz@gmail.com>
2014-05-11 22:03:26 +0200

    ProgramDao: query moved to db view

--- app-dev-root/src/PA032/Bundle/MultikinoBundle/DAO/ProgramDAO.php
+++ app-dev-root/src/PA032/Bundle/MultikinoBundle/DAO/ProgramDAO.php
@@ -8,9 +8,22 @@ use IBA\Bundle\DAOBundle\DAO\DAOBaseImpl;
 
 class ProgramDAO extends DAOBaseImpl
 {
-	
+
+	public function getById($projectionId)
+	{
+		$statement = $this->db->prepare('
+			SELECT *
+			FROM projection_overview
+			WHERE
+				projection_id = :projection_id
+		');
+		$statement->bindParam('projection_id', $projectionId);
+		$statement->execute();
+		return $this->hydrate($statement->fetchObject());
+	}
+
+
 	/**
-	 * 
 	 * @param \Date $date
 	 * @param int $branchOfficeId
 	 * @return ProgramRecordVO[]
@@ -20,101 +33,46 @@ class ProgramDAO extends DAOBaseImpl
 		$projectionStartFrom = $date->format("Y-m-d H:i:s");
 		date_add($date, date_interval_create_from_date_string('1 day'));
 		$projectionStartTo = $date->format("Y-m-d H:i:s");
-		
+
 		$records = array();
-		try
-		{
-			$stmt = $this->db->prepare('
-					SELECT
-				  projection.projection_id as "projection_id",
-				  movie.title AS "title",
-				  movie.descritption AS "descritiption",
-				  movie.running_time AS "running_time",
-				  date_part(\'year\', movie.release_date) AS "release_year",
-				  projection.start AS "projection_start_time",
-				  projection.price AS "projection_price",
-				  hall.hall_id AS "hall_id",
-				  (SELECT
-				    array(
-				      SELECT
-				        genre.genre_type
-				      FROM
-				        movie_genre
-				        LEFT JOIN genre
-				          ON genre.genre_id = movie_genre.genre_id
-				      WHERE
-				        movie.movie_id = movie_genre.movie_id
-				    )
-				  ) AS "genre_types",
-				  (
-				  SELECT
-				    COUNT(*)
-				  FROM
-				    hall_seat
-				    LEFT JOIN projection pr
-				      ON pr.hall_id = hall_seat.hall_id
-				  WHERE
-				    pr.projection_id = projection.projection_id
-				  ) AS "seats_total",
-				  (
-				  SELECT
-				    COUNT(*)
-				  FROM
-				    booking
-				    LEFT JOIN projection pr
-				      ON pr.projection_id = booking.projection_id
-				  WHERE
-				    pr.projection_id = projection.projection_id
-				  ) AS "seats_booked"
-				  
-				FROM 
-				  projection
-				  LEFT JOIN movie
-				    ON movie.movie_id = projection.movie_id
-				  LEFT JOIN hall
-				    ON hall.hall_id = projection.hall_id
-				  LEFT JOIN branch_office
-				    ON branch_office.branch_office_id = hall.branch_office_id
-				WHERE
-				  branch_office.branch_office_id = :branch_office_id
-				  AND projection.start >= :projection_start_from 
-				  AND projection.start < :projection_start_to
-				ORDER BY
-				  hall.label,
-				  projection.start
-					');
-			$stmt->bindParam(':branch_office_id', $branchOfficeId);
-			$stmt->bindParam(':projection_start_from', $projectionStartFrom);
-			$stmt->bindParam(':projection_start_to', $projectionStartTo);
-			$stmt->execute();
-			
-			
-			while ($row = $stmt->fetch(\PDO::FETCH_ASSOC, \PDO::FETCH_ORI_NEXT))
-			{
-				$record = new ProgramRecordVO();
-				$record->projectionId = $row["projection_id"];
-				$record->title = $row["title"];
-				$record->descritiption = $row["descritiption"];
-				$record->runningTime = $row["running_time"];
-				$record->releaseYear = $row["release_year"];
-				$record->projectionStartTime = new \DateTime($row["projection_start_time"]);
-				$record->projectionPrice = $row["projection_price"];
-				$record->hallId = $row["hall_id"];
-				$record->genreTypes = $row["genre_types"];
-				$record->seatsTotal = $row["seats_total"];
-				$record->seatsBooked = $row["seats_booked"];
-				$record->minuteOffsetFromMidnight = $record->projectionStartTime->format("H") * 60 + $record->projectionStartTime->format("i");
-				array_push($records, $record);
-			}
-	
+
+		$stmt = $this->db->prepare('
+			SELECT *
+			FROM projection_overview
+			WHERE
+				branch_office_id = :branch_office_id
+				AND start >= :projection_start_from
+				AND start < :projection_start_to
+		');
+		$stmt->bindParam(':branch_office_id', $branchOfficeId);
+		$stmt->bindParam(':projection_start_from', $projectionStartFrom);
+		$stmt->bindParam(':projection_start_to', $projectionStartTo);
+		$stmt->execute();
+
+		while ($row = $stmt->fetchObject()) {
+			$records[] = $this->hydrate($row);
 		}
-		catch (\Exception $e)
-		{
-			throw new \Exception($e);
-		}
-	
+
 		return $records;
 	}
-}
 
-?>
+
+	private function hydrate(\stdClass $row)
+	{
+		$record = new ProgramRecordVO();
+		$record->projectionId = $row->projection_id;
+		$record->title = $row->title;
+		$record->descritiption = $row->descritiption;
+		$record->runningTime = $row->running_time;
+		$record->releaseYear = $row->release_year;
+		$record->projectionStartTime = new \DateTime($row->start);
+		$record->projectionPrice = $row->projection_price;
+		$record->hallId = $row->hall_id;
+		$record->genreTypes = $row->genre_types;
+		$record->seatsTotal = $row->seats_total;
+		$record->seatsBooked = $row->seats_booked;
+		$record->minuteOffsetFromMidnight = (int) ($record->projectionStartTime->format("H") * 60) + (int) $record->projectionStartTime->format("i");
+
+		return $record;
+	}
+}
